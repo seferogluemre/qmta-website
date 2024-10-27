3576b12c816e05e6330bfb795ecb2d2be2040f02
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-10-27 16:59:56 +0300

    css fourth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ indicators.forEach((indicator) => {
     indicators.forEach((ind) => {
       ind.style.textShadow = "none";
       ind.style.boxShadow = "none";
-      ind.style.background = "transparent";
+      ind.style.background = "none";
     });
 
     const targetSection = document.querySelector(
@@ -13,7 +13,7 @@ indicators.forEach((indicator) => {
     );
 
     indicator.style.boxShadow = "0 0 55px rgb(255, 255, 255)";
-    indicator.style.background = "rgba(0, 0, 0, 0.2)";
+    indicator.style.background = "rgba(0, 0, 0, 0)";
 
     const scrollToSection = (target, offset = 0) => {
       const targetPosition =
@@ -26,4 +26,37 @@ indicators.forEach((indicator) => {
 
     scrollToSection(targetSection, 30);
   });
+});
+
+// Hover
+const rightColumn = document.getElementById("right-column");
+const rightCharacterImage = document.querySelector(".right-character-image");
+const btn1 = document.querySelector(".hover-btn-two");
+
+rightColumn.addEventListener("mouseover", () => {
+  rightCharacterImage.style.filter = "none";
+  rightCharacterImage.style.transition = "0.9s ease-in-out";
+  btn1.style.background = "#55983d";
+  btn1.style.transition = "0.9s ease-in-out";
+});
+
+rightColumn.addEventListener("mouseout", () => {
+  btn1.style.background = "transparent";
+  rightCharacterImage.style.filter = "grayscale(80%) contrast(60%)";
+});
+
+const leftColumn = document.getElementById("left-column");
+const leftCharacterImage = document.querySelector(".left-character-image");
+const btn2 = document.querySelector(".hover-btn-one");
+
+leftColumn.addEventListener("mouseover", () => {
+  leftCharacterImage.style.filter = "none";
+  leftCharacterImage.style.transition = "0.9s ease-in-out";
+  btn2.style.background = "#55983d";
+  btn2.style.transition = "0.9s ease-in-out";
+});
+
+leftColumn.addEventListener("mouseout", () => {
+  btn2.style.background = "transparent";
+  leftCharacterImage.style.filter = "grayscale(80%) contrast(60%)";
 });
