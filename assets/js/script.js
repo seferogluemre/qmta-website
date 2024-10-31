0d9142ed5563bcabf178bed561e4dfb00b475931
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-11-01 02:30:16 +0300

    responsive design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,3 @@
-const indicators = document.querySelectorAll(".indicator");
-
-indicators.forEach((indicator) => {
-  indicator.addEventListener("click", (e) => {
-    indicators.forEach((ind) => {
-      ind.style.textShadow = "none";
-      ind.style.boxShadow = "none";
-      ind.style.background = "none";
-    });
-
-    const targetSection = document.querySelector(
-      indicator.getAttribute("data-target")
-    );
-
-    indicator.style.boxShadow = "0 0 55px rgb(255, 255, 255)";
-    indicator.style.background = "rgba(0, 0, 0, 0)";
-
-    const scrollToSection = (target, offset = 0) => {
-      const targetPosition =
-        target.getBoundingClientRect().top + window.pageYOffset - offset;
-      window.scrollTo({
-        top: targetPosition,
-        behavior: "smooth",
-      });
-    };
-
-    scrollToSection(targetSection, 30);
-  });
-});
-
 // Hover
 const rightColumn = document.getElementById("right-column");
 const rightCharacterImage = document.querySelector(".right-character-image");
@@ -35,9 +5,9 @@ const btn1 = document.querySelector(".hover-btn-two");
 
 rightColumn.addEventListener("mouseover", () => {
   rightCharacterImage.style.filter = "none";
-  rightCharacterImage.style.transition = "0.3s ease-in";
+  rightCharacterImage.style.transition = "0.1s ease-in";
   btn1.style.background = "#B03052";
-  btn1.style.transition = "0.3s ease-in";
+  btn1.style.transition = "0.1s ease-in";
 });
 
 rightColumn.addEventListener("mouseout", () => {
@@ -51,11 +21,11 @@ const btn2 = document.querySelector(".hover-btn-one");
 
 leftColumn.addEventListener("mouseover", () => {
   leftCharacterImage.style.filter = "none";
-  leftCharacterImage.style.transition = "0.3s ease-in";
+  leftCharacterImage.style.transition = "0.1s ease-in";
   btn2.style.background = "#B03052";
-  btn2.style.transition = "0.3s ease-in";
+  btn2.style.transition = "0.1s ease-in";
 });
-
+1;
 leftColumn.addEventListener("mouseout", () => {
   btn2.style.background = "transparent";
   leftCharacterImage.style.filter = "grayscale(100%)";
